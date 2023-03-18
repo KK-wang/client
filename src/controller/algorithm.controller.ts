54b9e9bf3f7848b829c1d5184659838d3f9e2c70
lorem <1021448137@qq.com>
2023-03-18 14:19:59 +0800

    feat: update createAlgorithmTask api param

--- src/controller/algorithm.controller.ts
+++ src/controller/algorithm.controller.ts
@@ -4,24 +4,24 @@ import { PythonShell as py } from "python-shell";
 
 interface IRequestBody {
   algorithm: string,
-  data: {
-    nodes: {
-      nodeName: string,
-      cpu: number,
-      mem: number,
-    }[],
-    tasks: {
-      podName: string,
-      image: string,
-    }[]
-  }
+  nodes: {
+    nodeName: string,
+    cpu: number,
+    mem: number,
+  }[],
+  tasks: {
+    podName: string,
+    image: string,
+    nums: number,
+    calcMetrics: string,
+  }[]
 }
 
 async function createAlgorithmTask(ctx: Koa.ParameterizedContext, next: Koa.Next) {
-  const { algorithm, data } = ctx.request.body as IRequestBody;
+  const { algorithm, nodes, tasks } = ctx.request.body as IRequestBody;
   const res = await py.run(path.resolve(__dirname, `../config/algorithm/${algorithm}alg.py`), {
     mode: "json",
-    args: [JSON.stringify(data)], // 传递 JSON 参数。
+    args: [JSON.stringify({ nodes, tasks })], // 传递 JSON 参数。
   });
   ctx.body = {
     scheduling: res[0]
