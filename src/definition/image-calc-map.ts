b202e3a49b05b43b0532d2a0d8116287f9e65a7d
lorem <1021448137@qq.com>
2023-04-07 17:14:13 +0800

    feat: update calc data

--- src/definition/image-calc-map.ts
+++ src/definition/image-calc-map.ts
@@ -1,12 +1,12 @@
 const imageCalcMap: {
   [key: string]: string
 } = {
-  "ai-task-5s": "5000",
-  "ai-task-7s": "7000",
-  "ai-task-9s": "9000",
-  "ai-task-11s": "11000",
-  "ai-task-13s": "13000",
-  "ai-task-15s": "15000",
+  "ai-task-5s": "121",
+  "ai-task-7s": "169",
+  "ai-task-9s": "218",
+  "ai-task-11s": "267",
+  "ai-task-13s": "315",
+  "ai-task-15s": "364",
 }
 
 export default imageCalcMap;
