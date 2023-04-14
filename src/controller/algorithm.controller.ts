a98494f05c71e3182d2cebd3dab40d49b2b80846
lorem <1021448137@qq.com>
2023-04-14 18:56:56 +0800

    feat: add Solution interface

--- src/controller/algorithm.controller.ts
+++ src/controller/algorithm.controller.ts
@@ -17,15 +17,21 @@ interface IRequestBody {
   }[]
 }
 
+interface ISolution {
+  podName: string,
+  image: string,
+  nodeName: string,
+}
+
+type Solution = ISolution[]
+
 async function createAlgorithmTask(ctx: Koa.ParameterizedContext, next: Koa.Next) {
   const { algorithm, nodes, tasks } = ctx.request.body as IRequestBody;
   const res = await py.run(path.resolve(__dirname, `../config/algorithm/${algorithm}alg.py`), {
     mode: "json",
     args: [JSON.stringify({ nodes, tasks })], // 传递 JSON 参数。
   });
-  ctx.body = {
-    scheduling: res[0]
-  };
+  ctx.body = res[0] as Solution;
 }
 
 export {
